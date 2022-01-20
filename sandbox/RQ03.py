4674131fa871d178ee75f46ed6877d88a48770bb
jacob brown <jsbrown@unc.edu>
2022-01-19 22:22:35 -0500

    if than else practice

---
+++ sandbox/RQ03.py
@@ -0,0 +1,14 @@
+"""challenge question 3"""
+
+choice: int = int(input("Enter a number: "))
+
+if choice > 50:
+    if choice > 75:
+        print("A")
+    else:
+        print("B")
+else:
+    if choice < 25: 
+        print("C")
+    else:
+        print("D")
